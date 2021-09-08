c305d193974db849f01132cb3a9fe9b582fe3037
Joseph Kristiano <o.joseph.k@gmail.com>
2021-09-08 14:12:37 +0700

    W04 - The VIEW

--- app/src/main/java/com/joseph18/ifubaya/advweek4/view/MainActivity.kt
+++ app/src/main/java/com/joseph18/ifubaya/advweek4/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.joseph18.ifubaya.advweek4
+package com.joseph18.ifubaya.advweek4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joseph18.ifubaya.advweek4.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
